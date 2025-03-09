5a1e6ba3e6f0da8ac06a2f7de2d89be7ce7d59e3
timon.derboven <timon.derboven@uzh.ch>
2025-03-09 23:33:32 +0100

    Implement Prompting techniques #6
    -promting technique enum
    -abc
    -first cot version

--- src/api_handler.py
+++ src/api_handler.py
@@ -4,7 +4,7 @@ from langchain.schema import HumanMessage
 class APIHandler:
     def __init__(self, api_key):
         self.api_key = api_key
-        self.chat_model = ChatOpenAI(model_name="gpt-4", openai_api_key=self.api_key)
+        self.chat_model = ChatOpenAI(model_name="gpt-4o", openai_api_key=self.api_key)
 
     def get_response(self, prompt):
         response = self.chat_model([HumanMessage(content=prompt)])
---
+++ src/thinking_hats_ai/prompting_techniques/chain_of_thought.py
@@ -0,0 +1,12 @@
+from langchain.prompts import PromptTemplate
+from thinking_hats_ai.prompting_techniques.base_technique import BasePromptingTechnique
+
+class ChainOfThought(BasePromptingTechnique):
+    def generate_prompt(self, input_text, hat_instructions):
+        template = PromptTemplate(
+            input_variables=["hat_instructions", "input_text"],
+            template="Imagine you wear a hat with the following instructions: {hat_instructions}\\n\\n"
+                    "This is the current state of the brainstorming: {input_text}"
+                    "What would you add from the perspective of the given hat? Justify your answer and give reasoning about you thought process step-by-step:\\n"
+        )
+        return template.format(hat_instructions=hat_instructions, input_text=input_text)
