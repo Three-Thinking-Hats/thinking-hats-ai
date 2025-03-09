3cae0670e9cf6541b27b206ac368868dcabc7c1d
timon.derboven <timon.derboven@uzh.ch>
2025-03-09 23:18:43 +0100

    Added API Handler
    -api handler using langchain
    -model gpt 4o
    
    Issue: #6

---
+++ src/api_handler.py
@@ -0,0 +1,11 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+
+class APIHandler:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.chat_model = ChatOpenAI(model_name="gpt-4", openai_api_key=self.api_key)
+
+    def get_response(self, prompt):
+        response = self.chat_model([HumanMessage(content=prompt)])
+        return response.content
