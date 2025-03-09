513523a602bc7f8897029efa3f5fc2cdc76c96f6
timon.derboven <timon.derboven@uzh.ch>
2025-03-09 23:53:04 +0100

    Add init.py for package import #6

--- src/thinking_hats_ai/__init__.py
+++ src/thinking_hats_ai/__init__.py
@@ -1,2 +1,5 @@
-def main() -> None:
-    print("Hello from thinking-hats-ai!")
+from .brainstorming_session import BrainstormingSession
+from .hats.hats import Hat
+from .prompting_techniques.technique import Technique
+
+__all__ = ["BrainstormingSession", "Hat", "Technique"]
