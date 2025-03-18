1fea26cfabcd3073bcecf3d8f4bd824d71d3824e
Timon Derboven <tderboven10@gmail.com>
2025-03-18 16:08:27 +0100

    #9 input obj in base technique

--- src/thinking_hats_ai/prompting_techniques/base_technique.py
+++ src/thinking_hats_ai/prompting_techniques/base_technique.py
@@ -1,7 +1,15 @@
 from abc import ABC, abstractmethod
 
+from ..utils.api_handler import APIHandler
+from ..utils.brainstorming_input import BrainstormingInput
+
 
 class BasePromptingTechnique(ABC):
     @abstractmethod
-    def execute_prompt(self, input_text, hat_instructions):
+    def execute_prompt(
+        self,
+        brainstorming_input: BrainstormingInput,
+        hat_instructions: str,
+        api_handler: APIHandler,
+    ):
         pass
