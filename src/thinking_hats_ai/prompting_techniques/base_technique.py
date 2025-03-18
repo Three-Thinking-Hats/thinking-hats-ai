3f2b923162550a2743a1340686dcf7250e3e23a8
Twhining <120489751+Twhining@users.noreply.github.com>
2025-03-18 17:40:43 +0100

    Add logging and api_key from environment variables (#14)
    * Implement logging
    
    ---------
    
    Co-authored-by: Leon Braga <leonroberto.braga@uzh.ch>
    Co-authored-by: Timon Derboven <tderboven10@gmail.com>

--- src/thinking_hats_ai/prompting_techniques/base_technique.py
+++ src/thinking_hats_ai/prompting_techniques/base_technique.py
@@ -1,7 +1,13 @@
 from abc import ABC, abstractmethod
 
+from ..utils.logger import Logger
+
 
 class BasePromptingTechnique(ABC):
+    def __init__(self, dev):
+        technique_name = self.__class__.__name__.lower()
+        self.logger = Logger(technique_name, dev)
+
     @abstractmethod
     def execute_prompt(self, input_text, hat_instructions):
         pass
