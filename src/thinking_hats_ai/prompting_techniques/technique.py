33860b6528cfde7d21f66257e80d9bc16ef673e0
marctimothy.huber@uzh.ch <marctimothy.huber@uzh.ch>
2025-04-02 11:58:55 +0200

    #19 initialized chain of verification

--- src/thinking_hats_ai/prompting_techniques/technique.py
+++ src/thinking_hats_ai/prompting_techniques/technique.py
@@ -5,3 +5,4 @@ class Technique(Enum):
     CHAIN_OF_THOUGHT = "chain_of_thought"
     PERSONA_PATTERN = "persona_pattern"
     CONTRASTIVE_PROMPTING = "contrastive_prompting"
+    CHAIN_OF_VERIFICATION = "chain_of_verification"
