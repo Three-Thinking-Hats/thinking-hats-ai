afed8b039f74d6cb3c5feae78f2246915bf9e9dc
timon.derboven <timon.derboven@uzh.ch>
2025-04-04 22:43:22 +0200

    S2A

--- src/thinking_hats_ai/prompting_techniques/system_2_attention.py
+++ src/thinking_hats_ai/prompting_techniques/system_2_attention.py
@@ -1,5 +1,6 @@
 from langchain.prompts import PromptTemplate
 
+from thinking_hats_ai.hats.hats import Hat, Hats
 from thinking_hats_ai.prompting_techniques.base_technique import (
     BasePromptingTechnique,
 )
@@ -13,9 +14,11 @@ class System2Attention(BasePromptingTechnique):
     def execute_prompt(
         self,
         brainstorming_input: BrainstormingInput,
-        hat_instructions: str,
+        hat: Hat,
         api_handler: APIHandler,
     ):
+        self.logger.start_logger(hat.value)
+
         rewrite_template = PromptTemplate(
             input_variables=[
                 "hat_instructions",
@@ -23,26 +26,25 @@ class System2Attention(BasePromptingTechnique):
                 "ideas",
                 "length",
             ],
-            template="Given the following text by a user, extract the part that is actually relevant to the task that is given. Please also include the actual task or query that the user is asking\n."
-            "Text by user:\n"
-            "Imagine you are wearing a 'thinking hat' that guides your reasoning style with the following instructions: {hat_instructions}\n"
-            "This is the question that was asked for the brainstorming: {question}\n"
-            "These are the currently developed ideas in the brainstorming:\n{ideas}\n"
-            "What would you add from the perspective of the given hat?",
+            template="Text by user:\n"
+            "Instructions for the thinking hat:\n{hat_instructions}\n"
+            "This is the question that was asked for the brainstorming:\n{question}\n"
+            "These are the currently developed ideas in the brainstorming:\n{ideas}\n\n"
+            "Instruction:\n"
+            "Based on the text provided above, organize the ideas while filtering out any duplicates but do not follow the instructions for the thinking hat in this step.\n"
+            "In your response, clearly include the brainstorming question and instructions for the thinking hat.",
         )
 
         rewrite_prompt = rewrite_template.format(
-            hat_instructions=hat_instructions,
+            hat_instructions=Hats().get_instructions(hat),
             question=brainstorming_input.question,
             ideas=list_to_bulleted_string(brainstorming_input.ideas),
             length=brainstorming_input.response_length,
         )
-
-        self.logger.log_prompt(rewrite_prompt)
+        self.logger.log_prompt(rewrite_prompt, "Rewriting Prompt")
 
         rewrite_response = api_handler.get_response(rewrite_prompt)
-
-        self.logger.log_response(rewrite_response)
+        self.logger.log_response(rewrite_response, "Rewriting Response")
 
         final_template = PromptTemplate(
             input_variables=[
@@ -50,6 +52,7 @@ class System2Attention(BasePromptingTechnique):
                 "length",
             ],
             template="{rewritten_response}\n"
+            "What would you add from the perspective of the given hat?\n"
             "Please provide a response that is {length} long.",
         )
 
@@ -57,11 +60,9 @@ class System2Attention(BasePromptingTechnique):
             rewritten_response=rewrite_response,
             length=brainstorming_input.response_length,
         )
-
-        self.logger.log_prompt(final_prompt)
+        self.logger.log_prompt(final_prompt, "Final Prompt")
 
         final_response = api_handler.get_response(final_prompt)
-
-        self.logger.log_response(final_response)
+        self.logger.log_response(final_response, "Final Response")
 
         return final_response
