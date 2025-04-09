1324fe7085039dd19109e6742df37ac70fa081c7
Leon Braga <leonroberto.braga@uzh.ch>
2025-04-09 09:35:05 +0200

    Add react prompting

--- src/thinking_hats_ai/prompting_techniques/re_act.py
+++ src/thinking_hats_ai/prompting_techniques/re_act.py
@@ -1,12 +1,7 @@
-import os
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain_openai import ChatOpenAI
 
-from langchain.agents import initialize_agent
-from langchain.agents.agent_types import AgentType
-from langchain.chat_models import ChatOpenAI
-from langchain_community.tools import WikipediaQueryRun
-from langchain_community.utilities import WikipediaAPIWrapper
-
-from thinking_hats_ai.hats.hats import Hat
+from thinking_hats_ai.hats.hats import Hat, Hats
 from thinking_hats_ai.prompting_techniques.base_technique import (
     BasePromptingTechnique,
 )
@@ -24,26 +19,23 @@ class ReAct(BasePromptingTechnique):
         api_handler: APIHandler,
     ):
         input_str = (
-            f"You are wearing a thinking hat of color: {'Blue'}.\n"
-            "The color of the hat determines your perspective, responsibilities, and the way you think."
-            "in the ReAct phase you should find out what your color hat does. You should follow the react protocol."
-            "Your final answer should be your contribution to the brainstorming session throught the perspective of your hat.\n"
-            "Your job is to contribute to the brainstorming based on your hat's perspective.\n\n"
-            f"Brainstorming Question: {brainstorming_input.question}\n\n"
-            f"Current Ideas:\n{list_to_bulleted_string(brainstorming_input.ideas)}\n\n"
-            f"Your task: From the {'Blue'} HAT perspective, suggest what else could be added.\n"
-            f"Your response should be approximately {brainstorming_input.response_length} sentences long."
+            f"Imagine you wear a thinking hat, which leads your thoughts with the following instructions: {Hats().get_instructions(hat)}\n"
+            f"This is the question that was asked for the brainstorming: {brainstorming_input.question}\n"
+            f"These are the currently developed ideas in the brainstorming:\n{list_to_bulleted_string(brainstorming_input.ideas)}\n"
+            f"What would you add from the perspective of the given hat?\n"
         )
 
-        api_key = os.getenv("OPENAI_API_KEY")
         llm = llm = ChatOpenAI(
-            temperature=0, model_name="gpt-4o-mini", openai_api_key=api_key
+            temperature=0.8,
+            model_name="gpt-4o-mini",
+            api_key=api_handler.api_key,
         )
-        api_wrapper = WikipediaAPIWrapper(
-            top_k_results=1, doc_content_chars_max=100
+
+        tools = load_tools(
+            ["human"],
+            llm=llm,
         )
-        tool = WikipediaQueryRun(api_wrapper=api_wrapper)
-        tools = [tool]
+
         agent = initialize_agent(
             tools,
             llm,
